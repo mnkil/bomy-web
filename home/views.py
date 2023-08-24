bcd25d67a6c7e6c567414bd264d81e11ec8c4032
Michael Kilchenmann <michaelkilchenmann@Michaels-MacBook-Pro.local>
2023-08-24 23:20:48 +0800

    pic update

--- home/views.py
+++ home/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, HttpResponse
+from django.templatetags.static import static
 
 
 # Create your views here.
 def hello(request):
-    name="mad mike"
-    size=40
-    Interests=['mochi', 'degen crypto', 'beer']
-    return render(request, 'hello.html',{'name':name, 'size':size, 'Interests':Interests})
+    name = "mad mike"
+    size = 40
+    interests = ['mochi', 'degen crypto', 'beer']
 
+    image_path = 'tramdepot.jpeg'  # Path to the image within the static directory
+    image_url = static(image_path)  # Get the URL for the image
+
+    context = {
+        'name': name,
+        'size': size,
+        'interests': interests,
+        'image_url': image_url,
+    }
+
+    return render(request, 'hello.html', context)
