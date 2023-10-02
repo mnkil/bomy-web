70d80ed22c5e31b1f70a10d17b093757bf63df54
Michael Kilchenmann <michaelkilchenmann@Michaels-MacBook-Pro.local>
2023-10-02 20:33:14 +0800

    update

--- home/views.py
+++ home/views.py
@@ -22,25 +22,43 @@ def hello(request):
         df = pd.read_pickle(df_url)
 
     df.rename(columns={'Timestamp': 'timestamp'}, inplace=True)
-    dfchart = df[(df['market'] == 'ETH-USD') | (df['market'] == 'BTC-USD')]
     dft = df.tail(38)
     dft['apy'] = dft['apy'].multiply(100)
     dft['apy'] = dft['apy'].apply(lambda x: round(x,0))
     dft['fundingrate'] = dft['fundingrate'].multiply(10000)
-    dft['fundingrate'] = dft['fundingrate'].apply(lambda x: round(x,2))
+    dft['fundingrate'] = dft['fundingrate'].apply(lambda x: round(x,3))
+    dfchart = df[(df['market'] == 'ETH-USD') | (df['market'] == 'BTC-USD')]
+    dfchart['apy'] = dfchart['apy'].multiply(100)
+    dfchart['apy'] = dfchart['apy'].apply(lambda x: round(x,0))
+    dfchart['fundingrate'] = dfchart['fundingrate'].multiply(10000)
+    dfchart['fundingrate'] = dfchart['fundingrate'].apply(lambda x: round(x,3))
     json_records = dft.reset_index().to_json(orient='records')
     data = []
     data = json.loads(json_records)
-    json_records2 = dfchart.reset_index().to_json(orient='records')
-    cdata = []
-    cdata = json.loads(json_records2)
+    
+    # Filter data for BTC-USDD and ETH-USD
+    
+    df_btc = dfchart[dfchart['market'] == 'BTC-USD']
+    df_eth = dfchart[dfchart['market'] == 'ETH-USD']
+    
+    # Prepare data for BTC-USD and ETH-USD as lists
+    btc_data = df_btc[['timestamp', 'apy']].to_dict(orient='list')
+    eth_data = df_eth[['timestamp', 'apy']].to_dict(orient='list')
+    
+    # Convert data to JSON for passing to the template
+    btc_json = json.dumps(btc_data)
+    eth_json = json.dumps(eth_data)    
+    print('test')
+    print(btc_json)
+    print(eth_json)	
     context = {
         'name': name,
         'size': size,
         'interests': interests,
         'image_url': image_url,
-        'd' : data,
-	'c' : cdata
+        'd': data,
+        'btc_data': btc_json,
+        'eth_data': eth_json
     }
 
     return render(request, 'hello.html', context)
